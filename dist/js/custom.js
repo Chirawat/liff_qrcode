19058c5fe23adf99b63771f660199b35a7b7cb54
Chirawat <chirawat.not@gmail.com>
2021-11-21 11:33:06 +0700

    update spinner

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -10,29 +10,32 @@ async function main() {
 
     if (params.get("param") !== null) {
         const profile = await liff.getProfile();
-        
+
         //$("#userId").append(profile.userId)
 
         $.ajax({
             url: "https://docs.google.com/spreadsheets/d/1FXXk80ECzWbOiiNBXMngZyC79vjXZu8sDYLnvJQBVd0/export?format=csv",
             success: function(result) {
-                if(result.indexOf(profile.userId) > 1) {
+                if (result.indexOf(profile.userId) > 1) {
                     lines = result.split('\r\n');
-                    lines.map((data)=>{
+                    lines.map((data) => {
                         var field = data.split(',');
-                        if(field[0] === profile.userId){
+                        if (field[0] === profile.userId) {
 
                             $('#name').append(field[2] + field[3] + '  ' + field[4]);
                             var now = new Date();
                             $('#time').append(now.toLocaleTimeString());
+
+                            $('#spinner').prop('display: none;');
+
                             // get students data
-                            var url = 'https://docs.google.com/forms/d/e/1FAIpQLScH94bKiIR54tzzWDd8dkvvTka_TkHJjb5RS6Ka2YvuOtS_jQ/formResponse?usp=pp_url&entry.448259260=' + 
-                            profile.userId + '&entry.58029896=' +
-                            field[1] + '&entry.670271101=' +
-                            params.get("param") + '&entry.325908539=' +
-                            field[2] + field[3] + '  ' + field[4] + '&entry.718694407=' + 
-                            field[6] + '&entry.719468585=' +
-                            field[7] + '&submit=Submit';
+                            var url = 'https://docs.google.com/forms/d/e/1FAIpQLScH94bKiIR54tzzWDd8dkvvTka_TkHJjb5RS6Ka2YvuOtS_jQ/formResponse?usp=pp_url&entry.448259260=' +
+                                profile.userId + '&entry.58029896=' +
+                                field[1] + '&entry.670271101=' +
+                                params.get("param") + '&entry.325908539=' +
+                                field[2] + field[3] + '  ' + field[4] + '&entry.718694407=' +
+                                field[6] + '&entry.719468585=' +
+                                field[7] + '&submit=Submit';
 
                             $('#status').text('สำเร็จ!');
                             $("#close-button").css("display", "inline");
@@ -42,12 +45,11 @@ async function main() {
                             xmlHttp.open("GET", url, false); // false for synchronous request
                             xmlHttp.send(null);
 
-                            
+
                         }
                     });
-                    
-                }
-                else{
+
+                } else {
                     alert('ไม่พบบัญชี กรุณาลงทะเบียนก่อนใช้งาน')
                 }
             }
